b95ac1fe9274ebf92907aec851e2291051ada6fe
voltAntonio <avolta@arpae.it>
2020-09-23 13:09:26 +0200

    add files

---
+++ carbonCalculatorGeco2/main.cpp
@@ -0,0 +1,54 @@
+#include <QCoreApplication>
+#include <stdio.h>
+#include <math.h>
+
+#define NODATA -9999
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    // input
+    int cropType = 1;
+    int residueType = 1;
+    double residueWeight = 5;
+    // output
+    double kgCO2EqDueToN2O = NODATA;
+    double kgCO2EqDueToCO2 = NODATA;
+    double kgCO2EqDueToCH4 = NODATA;
+    double kgCO2EqTotal = NODATA;
+
+    double parameterEquivalentCH4 = 25;
+    double parameterEquivalentN2O = 296;
+    // parameters to be read from DB
+    double parameterDryMatterFraction = 0.9;
+    double parameterSlope = 0.29;
+    double parameterIntercept = 0;
+    double parameterAboveGroundContentN = 0.027;
+    double parameterBelowAboveRatio = 0.4;
+    double parameterBelowGroundContentN = 0.019;
+    int isTreeCrop = 1;
+
+    double parameterEmissionCH4 = 0.065333333;
+    double parameterEmissionN2O = 	0.00050675;
+    double parameterResidueReconvertedToCO2 = 10;
+
+    double aboveGroundNitrogen;
+    aboveGroundNitrogen = parameterAboveGroundContentN*residueWeight;
+    double belowGroundResidue;
+    belowGroundResidue = parameterBelowAboveRatio*residueWeight;
+    double belowGroundNitrogen = belowGroundResidue*parameterBelowGroundContentN;
+
+    double emissionCH4;
+    emissionCH4 = parameterEmissionCH4*residueWeight*1000;
+    double emissionN2O;
+    emissionN2O = parameterEmissionN2O*residueWeight*1000;
+
+    kgCO2EqDueToCH4 = emissionCH4 * parameterEquivalentCH4;
+    kgCO2EqDueToN2O = emissionN2O * parameterEquivalentN2O;
+
+
+
+    kgCO2EqDueToCO2 = 1000*(parameterDryMatterFraction*residueWeight*parameterResidueReconvertedToCO2/100.);
+    printf("%f %f %f\n",kgCO2EqDueToCH4,kgCO2EqDueToN2O,kgCO2EqDueToCO2);
+    return 0;
+}
