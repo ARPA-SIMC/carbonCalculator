9f9c06ed34778366b021919ed703f5a35daff0b9
voltAntonio <avolta@arpae.it>
2021-04-27 14:44:57 +0200

    aggiunta file training croazia

--- carbonCalculator/main.cpp
+++ carbonCalculator/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         #ifdef TEST
-            csvFileName = dataPath + "inputFileCroaziaDebug.csv";
+            csvFileName = dataPath + "inputDataTrainingCroatia.csv";
         #else
             usage();
             return 1;
