d52aedfba18560e3696ad046fb66bfb53e92c098
Fausto Tomei <ftomei@arpae.it>
2021-05-13 15:01:36 +0200

    update

--- carbonCalculatorGUI/mainwindow.cpp
+++ carbonCalculatorGUI/mainwindow.cpp
@@ -57,6 +57,7 @@ void MainWindow::on_actionChoose_sellers_triggered()
     ui->logBrowser->append("\nRead csv file: " + csvFileName);
     int numberOfExperiments = 0;
     std::vector<TinputData> inputData;
+    error = "";
     readCsvFileSeller(csvFileName, inputData, numberOfExperiments, error);
     if (error != "")
         ui->logBrowser->append(error);
@@ -78,6 +79,7 @@ void MainWindow::on_actionChoose_buyers_triggered()
     ui->logBrowser->append("\nRead csv file: " + csvFileName);
     int numberOfBuyers = 0;
     std::vector<TinputDataBuyer> inputDataBuyer;
+    error = "";
     readCsvFileBuyer(csvFileName, inputDataBuyer, numberOfBuyers, error);
     if (error != "")
         ui->logBrowser->append(error);
